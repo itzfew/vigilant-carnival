7785f0058ffde162f5883d945f38b22dcc9b3d86
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-08-16 09:16:22 +0530

    Update stream.js

--- pages/api/stream.js
+++ pages/api/stream.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
 
     ffmpeg()
       .input(concatFilePath)
-      .inputOptions(['-f concat', '-safe 00623cb6a, '-re'])
+      .inputOptions(['-f concat', '-safe 0', '-re'])
       .outputOptions(['-c:v copy', '-c:a aac', '-f flv'])
       .output(RTMP_URL)
       .on('start', () => {
